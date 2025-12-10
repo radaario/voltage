9b98cb06681a5f786bfeab5452b458a621961c59
ercanzirh <ercanzirh@radaar.io>
2025-12-10 13:03:29 +0300

    Logger fixed by bringing channelKey

--- packages/config/defaults.ts
+++ packages/config/defaults.ts
@@ -18,7 +18,7 @@ export const SYSTEM_DEFAULTS = {
 // Application defaults
 export const APP_DEFAULTS = {
 	name: "VOLTAGE",
-	version: "1.1.0",
+	version: "1.1.1",
 	env: "local",
 	protocol: "http",
 	host: "localhost",
